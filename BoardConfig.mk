c726c06d1812685cef67944d8c275f0b456e7785
PowerX-NOT <pugalg100@gmail.com>
2023-12-25 10:06:01 +0530

    laurel_sprout: set TW_MAX_BRIGHTNESS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,7 @@ TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
 TARGET_USES_MKE2FS := true
 TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
